067023adbde4104541c184d992e39c254846ebc0
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 <l.leroty@gmail.com>
2022-07-29 16:08:49 +0200

    main.h

--- main.h
+++ main.h
@@ -1,12 +1,6 @@
 #ifndef HEADER
 #define HEADER
 
-struct history
-{
-    char* commande; 
-    struct history *next;
-} hist;
-
 int execute (char **argv, char **env);
 char *checkFile(char *File);
 #endif
